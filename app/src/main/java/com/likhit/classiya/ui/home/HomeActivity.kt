d039906b826393ff05a7363cd615e72a20d6b2e6
likhit <likhit@udhaar.co>
2020-08-03 02:36:22 +0530

    Added logic for the drawer layout.

--- app/src/main/java/com/likhit/classiya/ui/home/HomeActivity.kt
+++ app/src/main/java/com/likhit/classiya/ui/home/HomeActivity.kt
@@ -1,12 +1,48 @@
 package com.likhit.classiya.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
 import com.likhit.classiya.R
+import com.likhit.classiya.base.BaseActivity
+import com.likhit.classiya.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        setupToolbar(getString(R.string.app_name), false)
+
+        val drawerToggle =
+            ActionBarDrawerToggle(this, binding.drawerLayout,binding.homeToolbar.toolbar, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
