e40c4ce40b89f76729512bdd964619da264096a9
likhit <likhit@udhaar.co>
2020-08-02 16:01:05 +0530

    [BaseActivity.kt]->Base activity for all activities. [BaseFragment.kt]->Base fragment for all fragments. Renamed MainActivity.kt to HomeActivity.kt

--- app/src/main/java/com/likhit/classiya/ui/home/HomeActivity.kt
+++ app/src/main/java/com/likhit/classiya/ui/home/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.likhit.classiya
+package com.likhit.classiya.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.likhit.classiya.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
