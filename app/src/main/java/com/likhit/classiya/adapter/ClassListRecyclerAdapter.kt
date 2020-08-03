2588ba312a2446d85322bc3ee07e3ba647e4ad65
likhit <likhit@udhaar.co>
2020-08-03 12:00:48 +0530

    Added ClassListRecyclerAdapter.

---
+++ app/src/main/java/com/likhit/classiya/adapter/ClassListRecyclerAdapter.kt
@@ -0,0 +1,35 @@
+package com.likhit.classiya.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.likhit.classiya.databinding.LayoutItemClassCardBinding
+
+class ClassListRecyclerAdapter() :
+    RecyclerView.Adapter<ClassListRecyclerAdapter.ClassListRecyclerViewHolder>() {
+
+    var listClass = mutableListOf<String>()
+    var layoutInflater: LayoutInflater? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClassListRecyclerViewHolder {
+        if(layoutInflater==null){
+            layoutInflater= LayoutInflater.from(parent.context)
+        }
+        return ClassListRecyclerViewHolder(LayoutItemClassCardBinding.inflate(layoutInflater!!,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return listClass.size
+    }
+
+    override fun onBindViewHolder(holder: ClassListRecyclerViewHolder, position: Int) {
+
+    }
+
+
+    class ClassListRecyclerViewHolder(val binding: LayoutItemClassCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+}
