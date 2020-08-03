d7d444dcd2ba8e0bfbf34e9954bbf765654d363a
likhit <likhit@udhaar.co>
2020-08-03 12:25:05 +0530

    Added recycler view in HomeActivity.kt.

--- app/src/main/java/com/likhit/classiya/ui/home/HomeActivity.kt
+++ app/src/main/java/com/likhit/classiya/ui/home/HomeActivity.kt
@@ -5,13 +5,18 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.likhit.classiya.R
+import com.likhit.classiya.adapter.ClassListRecyclerAdapter
 import com.likhit.classiya.base.BaseActivity
 import com.likhit.classiya.databinding.ActivityHomeBinding
 
 class HomeActivity : BaseActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var adapter: ClassListRecyclerAdapter
+    private var listClass = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +25,38 @@ class HomeActivity : BaseActivity() {
         setupToolbar(getString(R.string.app_name), false)
 
         val drawerToggle =
-            ActionBarDrawerToggle(this, binding.drawerLayout,binding.homeToolbar.toolbar, R.string.open, R.string.close)
+            ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                binding.homeToolbar.toolbar,
+                R.string.open,
+                R.string.close
+            )
         binding.drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        initView()
+
+    }
+
+
+    private fun initView() {
+
+        adapter = ClassListRecyclerAdapter()
+
+        listClass = mutableListOf()
+        listClass.add("Science")
+        listClass.add("Math")
+        listClass.add("English")
+        listClass.add("Science")
+        listClass.add("Math")
+        listClass.add("English")
+
+        binding.homeContentLayout.classListRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.homeContentLayout.classListRecyclerView.adapter = adapter
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
